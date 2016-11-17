eeccbdc9c166541d2ed0d2423148104f3436e046
laolangs <617288633@qq.com>
2016-11-17 23:30:20 +0800

    jdbcTemplate multi-datasource

---
+++ learn03/src/main/java/com/koala/carl/Application.java
@@ -0,0 +1,17 @@
+package com.koala.carl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by carl on 16-11-16.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
