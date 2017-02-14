81679872f57c896b8b3868bd48fddf0ed146ff14
laolangs <617288633@qq.com>
2017-02-14 18:23:05 +0800

    test

---
+++ learn07/src/main/java/com/fyxj/Learn07Application.java
@@ -0,0 +1,12 @@
+package com.fyxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learn07Application.class, args);
+	}
+}
